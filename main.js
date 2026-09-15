4855821b23500a5dfef2c6826d79d08df51195e6
Nezox212 <nezox0903@gmail.com>
2026-09-15 15:38:34 +0600

    LESSON1

---
+++ main.js
@@ -0,0 +1,21 @@
+var name = "Руслан"
+var age = 16
+var isStudent = true
+var nothing = null
+var undefined = undefined
+console.log("name: " + name)
+console.log("age: " + age)
+console.log("isStudent: " + isStudent)
+console.log("nothing: " + nothing)
+console.log("undefined: " + undefined)
+
+console.log( name +" : " + typeof (name))
+console.log( age +" : " + typeof (age))
+console.log( isStudent +" : " + typeof (isStudent))
+console.log( nothing +" : " + typeof (nothing))
+console.log( undefined +" : " + typeof (undefined))
+var person = {name, age, isStudent}
+console.log(person)
+console.log("Меня зовут " + person.name + " , мне " + person.age + " лет, " + "я являюсь студентов Geeks?: " + person.isStudent)
+let myFavoritesFilms = ["Breaking bad", "The walking dead", "The boys", "Dexter", "Fight club"]
+console.log("Мои любимые фильмы/сериалы: " + myFavoritesFilms)
